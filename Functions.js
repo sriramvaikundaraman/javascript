b24880e9827d35c048f19cec96aa49154cc10e62
Sriram <sriramvaikundaraman@gmail.com>
2023-11-25 12:50:13 +0530

    Function class added

--- Functions.js
+++ Functions.js
@@ -103,3 +103,50 @@ setTimeout(greetFunction,2000); // here we are just referencing a function. It w
 setTimeout(()=>{
     console.log("Varata mamey durrrrr........");
 }, 3000);
+
+
+
+/// Function class and traditional class
+
+// Constructor function
+class Animal{
+    constructor(name){
+        this.name = name;
+    }
+}
+
+function Bird(name) {
+    this.name = name;
+    this.sayHello = ()=>{
+        console.log("Hello iam ",this.name);
+    }
+}
+
+// Adding a method to the prototype
+Animal.prototype.sayHello = function() {
+    console.log("Hello, I'm a " + this.name);
+};
+
+Animal.prototype.title = "Animal";
+
+// Creating an instance
+var cat = new Animal("Cat");
+var dog = new Animal("Dog");
+
+var parrot = new Bird("Parrot");
+console.log(parrot);
+console.log(cat)
+
+// Accessing a method through the prototype chain
+cat.sayHello(); // Output: "Hello, I'm a Cat"
+
+parrot.sayHello();
+dog.sayHello();
+
+cat.sayHelloCat = ()=> console.log("cat specific function");
+
+cat.sayHelloCat();
+// dog.sayHelloCat();
+
+
+console.log(cat.title, dog.title);
